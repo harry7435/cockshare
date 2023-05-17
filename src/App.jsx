407aea4af0ef36ddcac0429b7053720616402b97
ikjun <harry960629@naver.com>
2023-05-17 17:11:01 +0900

    Refactor: vite react js로 템플릿 변경

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+/* eslint-disable*/
+
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav';
+
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Input from './pages/home/oper/Input';
+import Detail from './pages/detail/Detail';
+import Edit from './pages/home/oper/Edit';
+import { useAuthContext } from './hooks/useAuthContext';
+import styled from './App.css';
+
+function App() {
+  const { isAuthReady, user } = useAuthContext();
+
+  /**
+   * Navigate
+   *
+   */
+  return (
+    <div>
+      {isAuthReady ? (
+        <BrowserRouter>
+          <Nav></Nav>
+          <Routes>
+            {/* as-is 로그인 되있으면 홈으로, 아니라면 로그인 화면으로 이동 */}
+            {/* to-be 무조건 홈으로 이동 */}
+            <Route path="/" element={<Home />}>
+              {/* element={user
+                    ? <Home />
+                    : <Navigate replace={true} to="/login"/>}>  */}
+            </Route>
+            {/* 로그인이 되어있다면, 로그인 화면이나 회원가입 화면으로 가지 못하게... */}
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate replace={true} to="/" />}
+            ></Route>
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate replace={true} to="/" />}
+            ></Route>
+            {/* 로그인이 되어있지 않다면 글작성 못하게... */}
+            <Route
+              path="/input"
+              element={
+                user ? <Input /> : <Navigate replace={true} to="/login" />
+              }
+            ></Route>
+            {/* detail 페이지 */}
+            <Route
+              path="/detail/:id"
+              element={
+                user ? <Detail /> : <Navigate replace={true} to="/login" />
+              }
+            ></Route>
+            {/* edit 페이지 */}
+            <Route
+              path="/edit/:id"
+              element={
+                user ? <Edit /> : <Navigate replace={true} to="/login" />
+              }
+            ></Route>
+            {/* 없는 페이지 */}
+            <Route path="*" element={<div>없는 페이지에요</div>} />
+          </Routes>
+        </BrowserRouter>
+      ) : (
+        'loading...'
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,44 @@
+/* eslint-disable*/
+
+import { Link } from 'react-router-dom';
+import { useAuthContext } from '../hooks/useAuthContext';
+import { useLogout } from '../hooks/useLogout';
+
+import styled from './Nav.module.css';
+
+export default function Nav() {
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+
+  return (
+    <nav className={styled.nav}>
+      <h1 className={styled.tit}>
+        <Link to="/">My Mini Board</Link>
+      </h1>
+      <ul className={styled.list_nav}>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">로그인</Link>
+            </li>
+            <li>
+              <Link to="/signup">가입하기</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <li>
+            <strong>{user.displayName} 반가워요</strong>
+
+            <button type="button">
+              <Link to="/input">글작성</Link>
+            </button>
+            <button type="button" onClick={logout}>
+              로그아웃
+            </button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
